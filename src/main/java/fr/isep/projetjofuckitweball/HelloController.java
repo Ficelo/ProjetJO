5ce2b1cff6579e5f542cfc77094c84ac476177fc
Arsany9 <arsanydoudoutadros@gmail.com>
2024-04-24 19:47:59 +0200

    zdefdvf

--- src/main/java/fr/isep/projetjofuckitweball/HelloController.java
+++ src/main/java/fr/isep/projetjofuckitweball/HelloController.java
@@ -2,13 +2,16 @@ package fr.isep.projetjofuckitweball;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ImageView bannerView;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        // Charger l'image de la bannière
+        Image bannerImage = new Image("C:\\Users\\arsan\\IdeaProjects\\ProjetJO\\src\\main\\resources\\Images\\JoBan.jpg");
+        bannerView.setImage(bannerImage);
     }
 }
