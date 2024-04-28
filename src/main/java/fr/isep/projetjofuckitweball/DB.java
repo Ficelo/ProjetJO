0c2749aa8d5c31791fa3c4eec8fbb9df838a3f74
Arsany9 <arsanydoudoutadros@gmail.com>
2024-04-28 19:51:32 +0200

    la page de connexion et inscription marhce juste lors de la création jsp pk l'image par default ne marche pas askip c'est pas le bon chemin

---
+++ src/main/java/fr/isep/projetjofuckitweball/DB.java
@@ -0,0 +1,27 @@
+package fr.isep.projetjofuckitweball;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DB {
+    public Connection DbLink;
+
+    public Connection getConnection()
+    {
+        String databaseName="app_java";
+        String databaseUser="root";
+        String databasePassword="";
+        String url ="jdbc:mysql://localhost/"+databaseName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            DbLink =DriverManager.getConnection(url,databaseUser,databasePassword);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return DbLink;
+    }
+}
