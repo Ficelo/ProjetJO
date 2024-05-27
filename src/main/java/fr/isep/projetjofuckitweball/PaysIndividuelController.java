52b0b6438e4ae8c7928b5417d8a803bb7840fcb2
Ficelo <teiva.trillard@gmail.com>
2024-05-27 16:24:10 +0200

    Barres de recherche qui marche

--- src/main/java/fr/isep/projetjofuckitweball/PaysIndividuelController.java
+++ src/main/java/fr/isep/projetjofuckitweball/PaysIndividuelController.java
@@ -1,87 +1,26 @@
 package fr.isep.projetjofuckitweball;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.*;
 
-public class PaysController {
+public class PaysIndividuelController {
 
-    private Connection connection;
+
     public Label AcceuilButton;
     public Label SportButton;
     public Label PaysButton;
     public Label AdministrationButton;
     public Label RetourButton;
-    @FXML public TextField barreRecherche;
-    private String retourDestination = "";
+    public Text nomPays;
+    private String retourDestination;
 
-    public void initialize() {
-
-        initializeDB();
-
-        barreRecherche.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.ENTER) {
-                String query = "SELECT nom FROM pays";
-                try {
-                    PreparedStatement statement = connection.prepareStatement(query);
-                    ResultSet resultSet = statement.executeQuery();
-
-
-                    while (resultSet.next()) {
-                        String nom = resultSet.getString("nom");
-                        if(nom.equals(barreRecherche.getText())) {
-                            try {
-                                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PaysIndividuel.fxml"));
-                                Parent root = fxmlLoader.load();
-
-                                Scene scene = new Scene(root);
-
-                                Stage stage = (Stage) RetourButton.getScene().getWindow();
-
-                                stage.setScene(scene);
-                                stage.show();
-
-                                PaysIndividuelController controller = fxmlLoader.getController();
-                                controller.updateNomPays(barreRecherche.getText());
-
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void initializeDB() {
-        String databaseName = "app_java";
-        String databaseUser = "root";
-        String databasePassword = "";
-        String url = "jdbc:mysql://localhost/" + databaseName;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(url, databaseUser, databasePassword);
-            System.out.println("Connexion à la base de données établie.");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void loadConnexion(MouseEvent mouseEvent) {
         try {
@@ -170,6 +109,10 @@ public class PaysController {
 
     public void updateRetour(String retourDest){
         this.retourDestination = retourDest;
+    }
+
+    public void updateNomPays(String text) {
+        this.nomPays.setText(text);
     }
 
 }
--- src/main/java/fr/isep/projetjofuckitweball/SportController.java
+++ src/main/java/fr/isep/projetjofuckitweball/SportController.java
@@ -125,4 +125,5 @@ public class SportController {
         this.retourDestination = retourDest;
     }
 
+
 }
